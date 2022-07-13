f8717521b7af4236db5fbacf33f8c37da185808a
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-13 18:54:23 +0200

    Implemented Joke Controller

--- src/main/java/com/example/jokesapp/controllers/JokeController.java
+++ src/main/java/com/example/jokesapp/controllers/JokeController.java
@@ -1,2 +1,22 @@
-package com.example.jokesapp.controllers;public class JokeController {
+package com.example.jokesapp.controllers;
+
+import com.example.jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getJokes(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
 }
