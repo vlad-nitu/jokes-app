b81117d133264bc8e1a14bbcc7465bc3294dff70
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-13 18:53:18 +0200

    Implemented Services for Jokes

---
+++ src/main/java/com/example/jokesapp/controllers/JokeController.java
@@ -0,0 +1,2 @@
+package com.example.jokesapp.controllers;public class JokeController {
+}
---
+++ src/main/java/com/example/jokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.example.jokesapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
